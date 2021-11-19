c48e25e678bbdadfab97bb281329cc8905663b9d
FloLaeti <flo.laetitia@gmail.com>
2021-11-19 11:57:52 +0100

    creep v1

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import core
+import creep
 from creep import Creep
 
 
 def setup():
     print("setup START______")
     core.fps = 30
-    core.WINDOW_SIZE = [800,800]
+    core.WINDOW_SIZE = [800, 800]
+    print("setup END______")
 
     core.memory("listcreep", [])
-    core.memory("listcreep", 100)
-    for i in range(0, core.memory("listcreep")):
+    core.memory("nbcreep", 100)
+    for c in range(0, core.memory("nbcreep")):
         core.memory("listcreep").append(Creep())
+
+
+def run():
+    core.cleanScreen()
+    core.printMemory()
+
+    for c in core.memory("nbcreep"):
+        creep.show(core.screen)
+        creep.update()
 
 core.main(setup, run)
